81d037612ee94cdb2decf2488a7a7ff12326a71e
touhid1 <touhiduliislam5@gmail.com>
2020-10-04 22:08:49 +0600

    ok

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgaci.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsf87.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const app = express()
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+const port = 5000;
 
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+client.connect((err) => {
+   const collection = client.db("volunteer-network").collection("event");
+   app.post("/addEvent", (req, res) => {
+      const event = req.body;
+      collection.insertMany(event)
+      .then((result) => {
+         res.send(result.insertedCount > 0);
+      });
+   });
 
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const registrations = client.db("volunteerNetwork").collection("volunteerRegistrations");
-  
-  app.post('/addRegistration', (req,res) => {
-    const newRegistration = req.body;
-    registrations.insertOne(newRegistration)
-    .then(result => {
-      res.send(result.insertedCount > 0)
-    })
+   app.get('/events', (req, res) =>{
+      collection.find({})
+      .toArray( (err, documents) => {
+         res.send(documents);
+      })
   })
 });
 
-
-app.listen(5000, () => {
-  console.log(`Example app listening at http://localhost:5000`)
-})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
